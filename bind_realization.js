470507a7914a9b4d925cce06af1995b7a4e11200
Vitali <emailme1452@gmail.com>
2017-05-08 17:31:20 -0700

    added encapsulating example

--- bind_realization.js
+++ bind_realization.js
@@ -1,13 +1,19 @@
+Function.prototype.bind = function (context) {
+  //closure
+  var self = this;
+  return function () {
+    return self.apply(context, arguments);
+  }
+}
+
 var User = {
   count: 1,
 
   getCount: function () {
     return this.count;
   }
-};
-
-console.log(User.getCount());
+}
 
 var func = User.getCount.bind(User);
 
-console.log(func.call(User, null));
+console.log(func());
